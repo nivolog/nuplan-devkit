359b5c4ba0a6ed5c68512f54dd2e25db15227b9e
setra2014 <yakudza-rnd@mail.ru>
2023-04-15 20:04:01 +0300

    add cnn

--- nuplan/docker/run_docker.sh
+++ nuplan/docker/run_docker.sh
@@ -4,5 +4,5 @@ CURRENT_PATH="$PWD"
 # jupyter 8888
 # tensorboard 6006
 # nuplan board 5006
-docker run --name nuplan --privileged --gpus all -it -p 8888:8888 -p 6006:6006 -p 5006:5006\
-      -v ~/lab_work_space/nuScenes/nuplan_repo/datasets:/nuplan-devkit/nuplan/dataset:rw \nuplan
+docker run --name nuplan --privileged --gpus all -it -p 8888:8888 -p 6006:6006 -p 5006:5006 --shm-size=10.24gb\
+      -v ~/home/aleksey/nuplan-devkit/nuplan/dataset:/nuplan-devkit/nuplan/dataset:rw \nuplan
